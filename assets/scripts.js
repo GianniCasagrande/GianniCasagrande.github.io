7929f0ecb05f4837c89dba9535e733942791b6ad
Gianni Casagrande <gcasagrande@axonify.com>
2018-06-17 08:39:15 -0400

    WIP

--- assets/scripts.js
+++ assets/scripts.js
@@ -5,6 +5,19 @@ $(document).on("scroll", function(){
 				} else {
 			$("header").removeClass("shrink");
 		}
+
+		$('.grid-container.overlay').on("touchstart", function (e) {
+			'use strict'; //satisfy code inspectors
+			var link = $(this); //preselect the link
+			if (link.hasClass('hover')) {
+			    return true;
+			 } else {
+			   link.addClass('hover');
+			   $('.grid-container.overlay').not(this).removeClass('hover');
+			   e.preventDefault();
+			   return false; //extra, and to make sure the function has consistent return points
+			  }
+			});
 });
 
 
